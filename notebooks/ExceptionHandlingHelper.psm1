849452526dccdc7991b29f8f923fe82b00e9dbd4
Kevin Reust <kevin.reust@gmail.com>
2020-12-11 08:55:53 +0100

    Added Start-MemoryHungryFunction in ExceptionHandlingHelper Module

--- notebooks/ExceptionHandlingHelper.psm1
+++ notebooks/ExceptionHandlingHelper.psm1
@@ -5,17 +5,20 @@ function Connect-ThirdPartyServer ([string]$ComputerName) {
    
     [int]$script:ConnectToConnectionTryCounters += 1
 
+
     # Connect Succesfully every second time
     if ($script:ConnectToConnectionTryCounters % 2 -eq 0) {
         Write-Information "🟢 Succesfully connected to Server $ComputerName"
-    } else {
+    } 
+    
+    else {
         throw [System.ServiceModel.ServerTooBusyException]::New(('🔴 Server {0} is currently too busy to connect' -f $ComputerName)) 
     }
 }
 Export-ModuleMember -Function Connect-ThirdPartyServer
 
 function Start-MemoryHungryFunction {
-    Write-Information "Calculating Infinity..."
+    Write-Information "Calculating Infdinity..."
     Start-Sleep -Seconds 1.0
     throw [System.InsufficientMemoryException]::New()
 }
